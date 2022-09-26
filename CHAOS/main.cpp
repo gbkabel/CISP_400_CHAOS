d389668dce9ef2c5f4ae0f1aa7e810054679f887
gbkabel <gbkabel@yahoo.com>
2022-09-26 16:39:41 -0700

    struct push

---
+++ CHAOS/main.cpp
@@ -0,0 +1,15 @@
+// Include important C++ libraries here
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+// Make code easier to type with "using namespace"
+using namespace sf;
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
+
+
